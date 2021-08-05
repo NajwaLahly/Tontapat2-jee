c9942f2846dc70b79e35ae0cd67a53da9cad9f92
Valentin Venom <valentin.venom@gmail.com>
2021-08-05 16:57:42 +0200

    Changed template composition + started implementing Offre

---
+++ TontapatIDAO/src/main/java/fr/eql/ai109/tontapat/idao/OffreIDAO.java
@@ -0,0 +1,12 @@
+package fr.eql.ai109.tontapat.idao;
+
+import java.util.List;
+
+import fr.eql.ai109.tontapat.entity.Offre;
+import fr.eql.ai109.tontapat.entity.OffreSearch;
+import fr.eql.ai109.tontapat.entity.Utilisateur;
+
+public interface OffreIDAO extends GenericIDAO<Offre> {
+	public List<Offre> getAllByUtilisateur(Utilisateur utilisateur);
+	public List<Offre> getSearchResults(OffreSearch offreSearch);
+}
