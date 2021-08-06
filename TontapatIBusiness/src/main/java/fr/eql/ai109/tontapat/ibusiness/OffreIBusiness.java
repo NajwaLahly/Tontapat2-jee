aac258531d1b7f044e112649fdf82844f44a5f74
valvenom <valentin.venom@protonmail.com>
2021-08-06 11:25:22 +0200

    Created Offre index and detail page

--- TontapatIBusiness/src/main/java/fr/eql/ai109/tontapat/ibusiness/OffreIBusiness.java
+++ TontapatIBusiness/src/main/java/fr/eql/ai109/tontapat/ibusiness/OffreIBusiness.java
@@ -3,7 +3,14 @@ package fr.eql.ai109.tontapat.ibusiness;
 import java.util.List;
 
 import fr.eql.ai109.tontapat.entity.Offre;
+import fr.eql.ai109.tontapat.entity.OffreDTO;
+import fr.eql.ai109.tontapat.entity.OffreSearch;
+import fr.eql.ai109.tontapat.entity.Utilisateur;
 
 public interface OffreIBusiness {
-	public List<Offre> findAll();	
+	public List<Offre> findAll();
+	public List<Offre> findAllfromUtilisateur(Utilisateur utilisateur);
+	public Offre getById(int id);
+	public List<OffreDTO> getSearchResults(OffreSearch offreSearch);
+	
 }
--- TontapatWeb/src/main/java/fr/eql/ai109/tontapat/controller/OffreManagedBean.java
+++ TontapatWeb/src/main/java/fr/eql/ai109/tontapat/controller/OffreManagedBean.java
@@ -16,10 +16,24 @@ public class OffreManagedBean implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
+	private int id;
+	
 	@EJB
 	private OffreIBusiness offreIBusiness;
 	
 	public List<Offre> ShowAll() {
 		return offreIBusiness.findAll();
+	}
+	
+	public Offre ShowById(int id) {
+		return offreIBusiness.getById(id);
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
 	}
 }
