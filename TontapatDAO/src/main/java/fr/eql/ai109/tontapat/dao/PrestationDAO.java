bb936a52e672d3f1285f883d53c43076e929e05c
ElodieF1 <feliot.elodie@gmail.com>
2021-08-08 16:20:54 +0200

    ajout architecture prestation

---
+++ TontapatDAO/src/main/java/fr/eql/ai109/tontapat/dao/PrestationDAO.java
@@ -0,0 +1,14 @@
+package fr.eql.ai109.tontapat.dao;
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import fr.eql.ai109.tontapat.entity.Prestation;
+import fr.eql.ai109.tontapat.idao.AnnonceIDAO;
+import fr.eql.ai109.tontapat.idao.PrestationIDAO;
+
+@Remote(PrestationIDAO.class)
+@Stateless
+public class PrestationDAO extends GenericDAO<Prestation> implements PrestationIDAO{
+
+}
---
+++ TontapatIBusiness/src/main/java/fr/eql/ai109/tontapat/ibusiness/PrestationIBusiness.java
@@ -0,0 +1,7 @@
+package fr.eql.ai109.tontapat.ibusiness;
+
+import fr.eql.ai109.tontapat.entity.Prestation;
+
+public interface PrestationIBusiness {
+	public Prestation getById(int id);
+}
