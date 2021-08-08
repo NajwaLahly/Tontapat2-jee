19ff7b381504d1287b5681b200b3d5825e0a8370
valvenom <valentin.venom@protonmail.com>
2021-08-08 21:47:01 +0200

    Started implementing OffreSearch Managed Bean

---
+++ TontapatWeb/src/main/java/fr/eql/ai109/tontapat/controller/OffreSearchManagedBean.java
@@ -0,0 +1,10 @@
+package fr.eql.ai109.tontapat.controller;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "mbOffreSearch")
+@SessionScoped
+public class OffreSearchManagedBean {
+
+}
