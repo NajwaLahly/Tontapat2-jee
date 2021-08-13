8b758dae72d7552328bb9bb661a7fc8e9a5a7a0d
valvenom <valentin.venom@protonmail.com>
2021-08-13 13:42:29 +0200

    Added Prestation IBusiness method createFromOffreDTO

--- TontapatBusiness/src/main/java/fr/eql/ai109/tontapat/business/PrestationBusiness.java
+++ TontapatBusiness/src/main/java/fr/eql/ai109/tontapat/business/PrestationBusiness.java
@@ -5,19 +5,26 @@ import javax.ejb.Remote;
 import javax.ejb.Stateless;
 
 import fr.eql.ai109.tontapat.entity.Offre;
+import fr.eql.ai109.tontapat.entity.OffreDTO;
+import fr.eql.ai109.tontapat.entity.Prestation;
 import fr.eql.ai109.tontapat.ibusiness.PrestationIBusiness;
 import fr.eql.ai109.tontapat.idao.PrestationIDAO;
 
 @Remote(PrestationIBusiness.class)
 @Stateless
-public class PrestationBusiness implements PrestationIBusiness  {
+public class PrestationBusiness implements PrestationIBusiness {
 
 	@EJB
-	private PrestationIDAO prestationIdao;
+	private PrestationIDAO prestationIDAO;
 
 	@Override
 	public void createPrestationOffer(Offre offre) {
-		prestationIdao.createPrestationOffer(offre);		
+		prestationIDAO.createPrestationOffer(offre);
+	}
+
+	@Override
+	public Prestation createFromOffreDTO(OffreDTO offreDTO) {
+		return prestationIDAO.createFromOffreDTO(offreDTO);
 	}
 
 }
--- TontapatIBusiness/src/main/java/fr/eql/ai109/tontapat/ibusiness/PrestationIBusiness.java
+++ TontapatIBusiness/src/main/java/fr/eql/ai109/tontapat/ibusiness/PrestationIBusiness.java
@@ -1,9 +1,13 @@
 package fr.eql.ai109.tontapat.ibusiness;
 
 import fr.eql.ai109.tontapat.entity.Offre;
+import fr.eql.ai109.tontapat.entity.OffreDTO;
+import fr.eql.ai109.tontapat.entity.Prestation;
 
 public interface PrestationIBusiness {
 
 	public void createPrestationOffer(Offre offre);
+
+	public Prestation createFromOffreDTO(OffreDTO offreDTO);
 
 }
