69446050912b3dc9830d0824b091dc2956833651
valvenom <valentin.venom@protonmail.com>
2021-08-13 13:41:57 +0200

    Added + implemented Prestation IDAO method createFromOffreDTO

--- TontapatDAO/src/main/java/fr/eql/ai109/tontapat/dao/PrestationDAO.java
+++ TontapatDAO/src/main/java/fr/eql/ai109/tontapat/dao/PrestationDAO.java
@@ -1,8 +1,6 @@
 package fr.eql.ai109.tontapat.dao;
 
-
-
-
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Remote;
@@ -10,6 +8,8 @@ import javax.ejb.Stateless;
 import javax.persistence.Query;
 
 import fr.eql.ai109.tontapat.entity.Offre;
+import fr.eql.ai109.tontapat.entity.OffreDTO;
+import fr.eql.ai109.tontapat.entity.OffreSearch;
 import fr.eql.ai109.tontapat.entity.Prestation;
 import fr.eql.ai109.tontapat.entity.Terrain;
 import fr.eql.ai109.tontapat.idao.PrestationIDAO;
@@ -25,6 +25,43 @@ public class PrestationDAO  extends GenericDAO<Prestation> implements Prestation
 		//terrains = query.getResultList();
 		
 		
+	}
+
+	@Override
+	public Prestation createFromOffreDTO(OffreDTO offreDTO) {
+		Offre offre = offreDTO.getOffre();
+		OffreSearch search = offreDTO.getSearch();
+		Prestation prestation = new Prestation();
+		
+		prestation.setTerrain(search.getTerrain());
+		
+		prestation.setDateDebutInstallation(search.getDateDebut());
+		prestation.setDateFinInstallation(search.getDateDebut());
+		prestation.setDateDebutDesinstallation(search.getDateFin());
+		prestation.setDateFinDesinstallation(search.getDateFin());
+		
+		prestation.setDateDebut(search.getDateDebut());
+		prestation.setDateApportTroupeau(search.getDateDebut());
+		prestation.setDateFin(search.getDateFin());
+		prestation.setDateRecuperationTroupeau(search.getDateFin());
+		prestation.setDateReservation(new Date());
+		
+		prestation.setTypeInstallation(offre.isInstallationAssuree());
+		prestation.setFrequenceIntervention(offre.getFrequenceIntervention());
+		prestation.setTroupeau(offre.getTroupeau());
+		prestation.setOffre(offre);
+		
+		prestation.setFraisInstallation(offreDTO.getFraisInstallation());
+		prestation.setFraisIntervention(offreDTO.getFraisIntervention());
+		prestation.setFraisBetail(offreDTO.getFraisBetail());
+		prestation.setFraisService(offreDTO.getFraisService());
+		prestation.setTVA(offreDTO.getTVA());
+		prestation.setPrixTotal(offreDTO.getPrixTotal());
+		
+		prestation = add(prestation);
+		prestation.setNumReservation(prestation.getDateReservation() + "-" + prestation.getId());
+		prestation = update(prestation);
+		return prestation;
 	}
 
 
--- TontapatIDAO/src/main/java/fr/eql/ai109/tontapat/idao/PrestationIDAO.java
+++ TontapatIDAO/src/main/java/fr/eql/ai109/tontapat/idao/PrestationIDAO.java
@@ -3,11 +3,14 @@ package fr.eql.ai109.tontapat.idao;
 import java.util.List;
 
 import fr.eql.ai109.tontapat.entity.Offre;
+import fr.eql.ai109.tontapat.entity.OffreDTO;
 import fr.eql.ai109.tontapat.entity.Prestation;
 
 public interface PrestationIDAO extends GenericIDAO<Prestation> {
 
 	public void createPrestationOffer(Offre offre);
+
+	public Prestation createFromOffreDTO(OffreDTO offreDTO);
 	 
 
 
