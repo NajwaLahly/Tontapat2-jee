e4b13823e6602f18f378c81b909eadc785654cc7
valvenom <valentin.venom@protonmail.com>
2021-08-16 21:36:44 +0200

    Started implementing Proposition edit and display modals

--- TontapatWeb/src/main/java/fr/eql/ai109/tontapat/controller/PropositionManagedBean.java
+++ TontapatWeb/src/main/java/fr/eql/ai109/tontapat/controller/PropositionManagedBean.java
@@ -1,6 +1,7 @@
 package fr.eql.ai109.tontapat.controller;
 
 import java.io.Serializable;
+import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
@@ -10,6 +11,7 @@ import javax.faces.bean.SessionScoped;
 
 import fr.eql.ai109.tontapat.entity.Prestation;
 import fr.eql.ai109.tontapat.entity.Proposition;
+import fr.eql.ai109.tontapat.entity.Utilisateur;
 import fr.eql.ai109.tontapat.ibusiness.PropositionIBusiness;
 
 /**
@@ -25,6 +27,33 @@ public class PropositionManagedBean implements Serializable {
 	@EJB
 	private PropositionIBusiness propositionIBusiness;
 
+	private Date dateDebut;
+	public Prestation getCurrentPrestation() {
+		return currentPrestation;
+	}
+
+	public void setCurrentPrestation(Prestation currentPrestation) {
+		this.currentPrestation = currentPrestation;
+	}
+	
+	private int id;
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	private Date dateFin;
+	private boolean typeInstallation;
+	private float prixTotal;
+	private String description;
+
+	@ManagedProperty(value = "#{mbPrestation.prestation}")
+	private Prestation currentPrestation;
+	
 	public List<Proposition> showAllByPrestationId(int id) {
 		System.out.println("PRESTATION ID MB : " + id);
 		return propositionIBusiness.findAllByPrestationId(id);
@@ -34,4 +63,43 @@ public class PropositionManagedBean implements Serializable {
 		return propositionIBusiness.findById(id);
 	}
 
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	public boolean isTypeInstallation() {
+		return typeInstallation;
+	}
+
+	public void setTypeInstallation(boolean typeInstallation) {
+		this.typeInstallation = typeInstallation;
+	}
+
+	public float getPrixTotal() {
+		return prixTotal;
+	}
+
+	public void setPrixTotal(float prixTotal) {
+		this.prixTotal = prixTotal;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
