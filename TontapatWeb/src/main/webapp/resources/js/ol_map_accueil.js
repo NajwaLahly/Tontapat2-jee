3e8294c448b2286bd13ac9ddcaeaa7c330ccfd11
Najwa Lahly <najwa.lahly@gmail.com>
2021-08-17 22:48:06 +0200

    add page d'accueil

--- TontapatWeb/src/main/webapp/resources/js/ol_map_accueil.js
+++ TontapatWeb/src/main/webapp/resources/js/ol_map_accueil.js
@@ -64,6 +64,36 @@ var vectorMarkersLayerAnnonces = new ol.layer.Vector({
         return styleAnnonces;
       }
 	});
+	
+	
+/**
+ * Elements that make up the popup.
+ */
+const container = document.getElementById('popup');
+const content = document.getElementById('popup-content');
+const closer = document.getElementById('popup-closer');
+
+/**
+ * Create an overlay to anchor the popup to the map.
+ */
+const overlay = new ol.Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250,
+  },
+});
+
+/**
+ * Add a click handler to hide the popup.
+ * @return {boolean} Don't follow the href.
+ */
+closer.onclick = function () {
+  overlay.setPosition(undefined);
+  closer.blur();
+  return false;
+};
+
 
 const map = new ol.Map({
     target: 'map',
@@ -73,11 +103,34 @@ const map = new ol.Map({
     	source: new ol.source.OSM(),
     	})    
 	],
+	overlays: [overlay],
     view: new ol.View({
       	center: ol.proj.fromLonLat([5.0414701, 47.3215806]),
       	zoom: 9
       	})
     });
+
+/**
+ * Add a click handler to the map to render the popup.
+ */
+map.on('singleclick', function(evt) {
+  var name = map.forEachFeatureAtPixel(evt.pixel, function(feature) {
+    return feature.get('name');
+  })
+
+  if (name) {
+    container.style.display = "block";
+    var coordinate = evt.coordinate;
+	content.innerHTML = name;
+    overlay.setPosition(coordinate);
+  } else {
+    container.style.display = "none";
+  }
+});
+map.on('pointermove', function(evt) {
+  map.getTargetElement().style.cursor = map.hasFeatureAtPixel(evt.pixel) ? 'pointer' : '';
+});
+
 
  map.addLayer(vectorMarkersLayerOffres);  	
  map.addLayer(vectorMarkersLayerAnnonces);
